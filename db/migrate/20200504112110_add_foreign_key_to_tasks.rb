8ce50448f3b3eecee1f66c5d904ba4f971785911
Anastasiya <Nastya199529@mail.ru>
2020-05-04 17:29:58 +0400

    fix adding foreign keys at the database level

---
+++ db/migrate/20200504112110_add_foreign_key_to_tasks.rb
@@ -0,0 +1,6 @@
+class AddForeignKeyToTasks < ActiveRecord::Migration[6.0]
+  def change
+    add_foreign_key :tasks, :users, column: :author_id, primary_key: "id"
+    add_foreign_key :tasks, :users, column: :assignee_id, primary_key: "id"
+  end
+end
