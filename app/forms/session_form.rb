8cbc0b5502b058ef2e65f122f8875dba12b0e454
Anastasiya <Nastya199529@mail.ru>
2020-05-08 23:21:41 +0400

    rename the user_valid? method to user_check_validation

--- app/forms/session_form.rb
+++ app/forms/session_form.rb
@@ -10,7 +10,7 @@ class SessionForm
 
   validates :email, presence: true, format: { with: /\A\S+@.+\.\S+\z/ }
   validates :password, presence: true
-  validate :user_valid?
+  validate :user_check_validation
 
   def user
     User.find_by(email: email)
@@ -18,7 +18,7 @@ class SessionForm
 
   private
 
-  def user_valid?
+  def user_check_validation
     if user.blank? || !user.authenticate(password)
       errors.add(:email, "email or password doesn't match")
     end
