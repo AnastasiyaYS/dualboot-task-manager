e3ee3613e2d1483e64583292f55325ca28ae3cfd
Anastasiya <Nastya199529@mail.ru>
2020-05-11 15:42:53 +0400

    fix test for developers controller

--- test/controllers/web/developers_controller_test.rb
+++ test/controllers/web/developers_controller_test.rb
@@ -3,6 +3,7 @@
 require 'test_helper'
 
 class Web::DevelopersControllerTest < ActionController::TestCase
+  include AuthHelper
   test 'should get new' do
     get :new
     assert_response :success
@@ -11,6 +12,7 @@ class Web::DevelopersControllerTest < ActionController::TestCase
   test 'should post create' do
     post :create, params: { developer: attributes_for(:developer) }
     assert_response :redirect
-    assert :signed_in?
+    assert signed_in?
+    assert_equal current_user.type, 'Developer'
   end
 end
