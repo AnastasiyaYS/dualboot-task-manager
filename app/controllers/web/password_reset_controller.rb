0da985589350888c22e37fb5668ff7436a6f4777
Anastasiya <Nastya199529@mail.ru>
2020-07-02 00:32:17 +0400

    add permitted attributes of password reset request form, rewrite password reset service to stateless

--- app/controllers/web/password_reset_controller.rb
+++ app/controllers/web/password_reset_controller.rb
@@ -4,11 +4,15 @@ class Web::PasswordResetController < Web::ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:password_reset_request_form][:email])
-    PasswordResetRequestForm.new({ email: @user.email })
-    PasswordResetService.new(@user).send_password_reset if @user
-    flash[:notice] = 'E-mail sent with password reset instructions.'
-    redirect_to(new_session_path)
+    @password_reset = PasswordResetRequestForm.new(password_reset_params)
+    if @password_reset.valid?
+      @user = User.find_by_email(params[:password_reset_request_form][:email])
+      PasswordResetService.send_password_reset(@user) if @user
+      flash[:notice] = 'E-mail sent with password reset instructions.'
+      redirect_to(new_session_path)
+    else
+      render(:new)
+    end
   end
 
   def edit
@@ -26,8 +30,8 @@ class Web::PasswordResetController < Web::ApplicationController
       redirect_to(new_password_reset_path)
       return
     end
-    password_reset = PasswordResetForm.new({ id: params[:id] }.merge(user_params(@user)))
-    if !password_reset.valid?
+    @password_reset = PasswordResetForm.new({ id: params[:id] }.merge(user_params(@user)))
+    if !@password_reset.valid?
       flash[:notice] = 'Password reset failed. Please try again'
       redirect_to(new_password_reset_path)
     elsif @user.update(user_params(@user))
@@ -44,5 +48,9 @@ class Web::PasswordResetController < Web::ApplicationController
   def user_params(user)
     role = user.type.downcase
     params.require(role).permit(:password, :password_confirmation)
+  end
+
+  def password_reset_params
+    params.require(:password_reset_request_form).permit(:email)
   end
 end
--- app/services/password_reset_service.rb
+++ app/services/password_reset_service.rb
@@ -1,18 +1,16 @@
-class PasswordResetService
-  def initialize(user)
-    @user = user
-  end
+module PasswordResetService
+  class << self
+    def send_password_reset(user)
+      generate_token(:password_reset_token, user)
+      user.password_reset_sent_at = Time.zone.now
+      user.save!
+      UserMailer.forgot_password(user).deliver_now
+    end
 
-  def send_password_reset
-    generate_token(:password_reset_token, @user)
-    @user.password_reset_sent_at = Time.zone.now
-    @user.save!
-    UserMailer.forgot_password(@user).deliver_now
-  end
-
-  def generate_token(column, user)
-    begin
-      user[column] = SecureRandom.urlsafe_base64
-    end while User.exists?(column => user[column])
+    def generate_token(column, user)
+      begin
+        user[column] = SecureRandom.urlsafe_base64
+      end while User.exists?(column => user[column])
+    end
   end
 end
