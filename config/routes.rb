c34dd545b7c2d840d627a8358189682030f5237e
Anastasiya <Nastya199529@mail.ru>
2020-07-08 00:26:02 +0400

    fix 3 routes for image management

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,11 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tasks, only: [:index, :show, :create, :update, :destroy]
+      scope 'tasks' do
+        put ':id/attachment', to: 'tasks#attach_image'
+        delete ':id/attachment', to: 'tasks#remove_image'
+      end
       resources :users, only: [:index, :show]
-      put 'tasks(/:id)/attachment', to: 'tasks#attach_image'
-      delete 'tasks(/:id)/attachment', to: 'tasks#remove_image'
     end
   end
 
